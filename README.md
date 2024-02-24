d80795771403d628b1efcaee08bfc8c5d5a01673
Yudhvir Sirkissoon <yudhvir2002@gmail.com>
2024-02-24 12:33:14 +0200

    webhook change

--- README.md
+++ README.md
@@ -1,25 +1,28 @@
 # esp-loadshedding-times
+
 Get the loadshedding times for the day, and send a webhook via IFTTT to update your Google Calendar
 
 # Prerequisites:
-  - An IFTTT account
-  - A Google Account
-  - An EskomSePush API token (Instuctions to get the token are on their website)
-  - A Linux machine to run cronjobs
+
+- A Make Account - https://www.make.com
+- A Google Account
+- An EskomSePush API token (Instuctions to get the token are on their website) - https://eskomsepush.gumroad.com/l/api
+- A Linux machine to run cronjobs
 
 # STEPS:
-  - Download the above files to your desired file_location
-  - Create an IFTTT Applet to recieve a webhook (be sure to update the eskom.py file with the webhook key)
-  - the webhook should recieve two values:
-    - value1: the starting time
-    - value2: the ending time
-  - the Webhook name should be: receive_loadshedding_time
-  - Fill in the information required in the eskom.py file
-  - To find your area id run the find_location.py and input your area name, and your EskomSePush API token
-  - create the following cronjobs:
-    - SHELL=/bin/sh
-    - PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:file_location
-    - 45 * * * * /usr/bin/python3 file_location/eskom.py >> file_location/outputs.txt
-    - 57 23 * * * /usr/bin/python3 file_location/copy_to_all_logs.py
-    - 58 23 * * * rm file_location/log.txt
-    - 59 23 * * * echo "" > file_location/log.txt
+
+- Download the above files to your desired file_location
+- Go to https://www.make.com and create a scenario
+- Click More -> Import Blueprint and select the blueprint.json file
+- Go under the Google Calendar module and sign into your account
+- Go under the Webhook module and create a webhook
+- Under the webhook advanced settings allow json pass-through
+- Fill in the information required in the eskom.py file
+- To find your area id run the find_location.py and enter the required info
+- create the following cronjobs:
+  - SHELL=/bin/sh
+  - PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:file_location
+  - 45 \* \* \* \* /usr/bin/python3 file_location/eskom.py >> file_location/outputs.txt
+  - 57 23 \* \* \* /usr/bin/python3 file_location/copy_to_all_logs.py
+  - 58 23 \* \* \* rm file_location/log.txt
+  - 59 23 \* \* \* echo "" > file_location/log.txt
--- eskom.py
+++ eskom.py
@@ -1,24 +1,24 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 import json
 
 ############
 # Input
 area_id = "" # eskom area id, please see docs NOTE the string must be lowercase
-ifttt_key = "" # ifttt webhook key
 file_location = "" # folder location of log file
 esp_token = "" # EskomSePush API Token
+req = "" # Make webhook address
 ############
 
 payload = {}
 headers = {"token" : esp_token}
 date= datetime.today().strftime('%Y-%m-%d')
+tomorrow = (datetime.today() + timedelta(1)).strftime('%Y-%m-%d')
 
 url = "https://developer.sepush.co.za/business/2.0/api_allowance"
 response = requests.request("GET", url, headers=headers, data=payload)
 text = json.loads(response.text)
 count = int(text["allowance"]["count"])
-
 current_time = datetime.today().strftime('%H:%M')
 
 if count < 50:
@@ -35,18 +35,6 @@ if count < 50:
             if days[i]["date"] == date:
                 today = days[i]
                 break
-        stages = today["stages"]
-        stages = str(stages)
-        stages = stages.split("],")
-
-        largest_stage = ""
-
-        times_list = []
-
-        for i in stages:
-            i = i.replace("[", "").replace("]", "").replace(" ", "").replace("'", "")
-            times_list.append(i)
-
         fin_out = ""
 
         for i in events:
@@ -56,8 +44,8 @@ if count < 50:
             event_end_time=str(event_end).split("T")[1].split("+")[0][:-3]
             event_end_time_hour = event_end_time.split(":")[0]
             if event_end_time_hour > "12":
-                event_end_time = str(int(event_end_time_hour)-12)+":"+event_end_time[3::]+"pm"
-            #print(event_end_time)
+                event_end_time = str(int(event_end_time_hour)-12)+":"+event_end_time[3::]+" PM"
+
             file = open(file_location+"/log.txt","r+")
             lines = file.readlines()
             if date in event_start or date in event_end:
@@ -66,30 +54,39 @@ if count < 50:
                 outlier = event_end_time < event_start_time
                 stage = int(str(i["note"]).split(" ")[1])
                 fin_out = fin_out + "|stage: " + str(stage)
-                #print(event_start_time, event_end_time)
-                req = 'https://maker.ifttt.com/trigger/receive_loadshedding_time/with/key/'+ifttt_key+'?value1='+event_start_time+'&value2='+event_end_time
-                req_w_date = date+" - "+req
+
+                event_end_time_wo_date = event_end_time
+                event_start_time = date +" "+ event_start_time
+                if event_end_time_hour == "00":
+                    event_end_time = tomorrow +" "+ event_end_time
+                else:
+                    event_end_time = date +" "+ event_end_time
+
+                
+                var = {'start_time': event_start_time, 'end_time':event_end_time, 'end_time_no_date': event_end_time_wo_date}
+
+
+                log = "|"+var["start_time"]+"  -  "+var["end_time"]+"|"
                 if not currently_loadshedding:
                     if not outlier:
-                        if req_w_date+"\n" not in lines:
-                            requests.post(req)
-                            fin_out = fin_out  + " - "+event_start_time+"-"+event_end_time+" => request made| "
-                            file.write(req_w_date+'\n')
+                        if log+"\n" not in lines:
+                            requests.post(req, data=var)
+                            fin_out = fin_out  + " - "+event_start_time+"-"+event_end_time+" => request made|\n"
+                            file.write(log+'\n')
                         else:
-                            fin_out = fin_out + " - "+event_start_time+"-"+event_end_time+" => already there| "
-                    elif outlier and current_time > event_end_time:
-                        if req_w_date+"\n" not in lines:
-                            requests.post(req)
-                            fin_out = fin_out  + " - "+event_start_time+"-"+event_end_time+" => request made| "
-                            file.write(req_w_date+'\n')
+                            fin_out = fin_out + " - "+event_start_time+"-"+event_end_time+" => already there|\n"
+                    elif outlier and current_time > event_end_time_wo_date:
+                        if log+"\n" not in lines:
+                            requests.post(req, data=var)
+                            fin_out = fin_out  + " - "+event_start_time+"-"+event_end_time+" => request made|\n"
+                            file.write(log+'\n')
                         else:
-                            fin_out = fin_out + " - "+event_start_time+"-"+event_end_time+" => already there| "
+                            fin_out = fin_out + " - "+event_start_time+"-"+event_end_time+" => already there|\n"
+        
         if len(fin_out)<=0:
             fin_out = "no loadshedding"
         print(fin_out)
         file.close()
-    
-
     else:
         print("no loadshedding")
 else:
